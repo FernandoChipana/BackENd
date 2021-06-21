8205ba37017b240f81412745e0dc7d6fa7b9bb69
FernandoChipana <f3rm1ch3@hmail.com>
2021-06-20 22:56:38 -0500

    atributo column

--- BackEnd/Entities/fmedicos.cs
+++ BackEnd/Entities/fmedicos.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BackEnd.Entities
 {
@@ -12,8 +13,11 @@ namespace BackEnd.Entities
 		public String cmp { get; set; }
 		public String nomb { get; set; }
 		public String serv { get; set; }
+		[Column("Nº_ESP")]
 		public String NESP { get; set; }
-		public String DNI { get; set; }
+		
+		public String dni { get; set; }
+		[Column("FEC_NAC")]
 		public DateTime FECNAC { get; set; }
 		public String celu { get; set; }
 		public String email { get; set; }
